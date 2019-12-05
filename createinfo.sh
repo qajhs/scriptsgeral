59b334a75998f30f8524892a180c6d6f0abfda48
vss-dot <52868259+vss-dot@users.noreply.github.com>
2019-12-05 20:25:19 -0300

    Update createinfo.sh

--- createinfo.sh
+++ createinfo.sh
@@ -9,14 +9,8 @@ texto=`echo $1 | tr A-Z a-z| cut -d ":" -f2 | sed 's/\///g'`
 servidor=`echo $2 | tr A-Z a-z`
 dominio=$texto
 ##################################
-echo "###########################################################"
-echo "##                                                       ##"
-echo "##                                                       ##"
-echo "##                    I N F O . P H P                    ##"
-echo "##                                                       ##"
-echo "##                                                       ##"
-echo "###########################################################"
-echo "                                 [Feito por vitor.silveira]"
+
+echo "[Feito por vitor.silveira@endurance.com]"
 if [[ $1 == "" ]]; then
     echo -e "[$vermelho$bold+$normal] Por favor, insira um domínio/subdomínio válido"
     exit
@@ -26,26 +20,23 @@ if [[ $2 == "" ]]; then
     exit
 fi
 DIRDom=$(whmapi1 domainuserdata domain=${dominio} | grep documentroot | awk '{print $2}')
+
 arquivo=$DIRDom/info.php
-cat /etc/userdomains | grep ^${dominio} | awk '{print $2}' > /dev/null 2>&1
+cat /etc/userdomains| grep ^${dominio} | awk '{print $2}' > /dev/null 2>&1
 if [[ "$?" != "0" ]]; then
-    echo "Usuário não identificado"
+    echo -e "[$vermelho$bold+$normal] Usuário não identificado"
     exit
 fi
 cat /etc/userdomains | grep ^${dominio} > /dev/null 2>&1
 if [[ "$?" != "0" ]]; then
-    echo "Dominio/Sub Dominio nao encontrado"
+    echo -e "[$vermelho$bold+$normal] Dominio/Sub Dominio nao encontrado"
     exit
 fi
-cd /tmp
-arquivocat=`mktemp cat.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`
-catt=$arquivocat
-identificado2=$(cat /var/named/${dominio}.db 2> ${catt})
+cat /var/named/${dominio}.db > /dev/null 2>&1
 if [[ "$?" != "0" ]]; then
-    echo -e "Dominio$vermelho nao$normal encontrado no servidor ${variavel2}"
+    echo -e "[$vermelho$bold+$normal] Dominio$vermelho nao$normal encontrado no servidor ${variavel2}"
     exit
 fi
-rm -f /tmp/$arquivocat
 if [[ -f $DIRDom/info.php ]]; then
     echo -e "[$vermelho$bold+$normal] Arquivo já existe"
     exit
